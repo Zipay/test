13c5cb38fb2efa36255272f4f46638bf2b84007f
GilangGR7 <restuayah2@gmail.com>
2023-01-14 09:55:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
-  token: "MTAyMzIzNTg1MTE5NjU3OTg5MA.GyvVne.iJSNcXfKFfIDWCfORtVf0x2KuFLvZD7Ax19bhc",
+  token: "MTAyMzIzNTg1MTE5NjU3OTg5MA.GanExE.3212zaR7qJdbiCoe7EnaYuE4vE7f2tT-nahuso",
   channelId: "1062338179249746002",
   timezone: "Asia/Jakarta",
 };
